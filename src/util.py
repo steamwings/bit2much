aaf395084ebfa6f183c8ab2a26ce80ee1d48f3b0
Zander Weatherford <zweatherford@outlook.com>
2018-05-04 14:30:26 -0400

    Fix issue in util.pywhich throws an exception for many integer values passed to int2ba

--- src/util.py
+++ src/util.py
@@ -4,18 +4,20 @@ import binascii
 verbose = False
 little_endian = (sys.byteorder == 'little')
 
-
 # bytearray OR string (to hex string then) to integer
 # Switches byte order for Little Endian systems unless ntoh=False
-def ba_to_int(bytearr, ntoh=True):
+def ba2int(bytearr, ntoh=True):
     if str(bytearr) == '': return 0;
     if ntoh and little_endian: 
         bytearr.reverse()
     return int(binascii.hexlify(bytearr), 16)
 
 # integer (to hex string then) to byte array
-def int_to_ba(n, hton=True):
-    ba = bytearray(binascii.unhexlify(format(n,'x')))
+def int2ba(n, hton=True):
+    f = format(n,'x')
+    if len(f) % 2 == 1: # Need even number of digits for octets
+        f = '0' + f
+    ba = bytearray(binascii.unhexlify(f))
     if hton and little_endian:
         ba.reverse()
     return ba
